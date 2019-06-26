77d5d7150f2ef6b602a309bf3a4dbf5536454b9d
ingako <hwu344@aucklanduni.ac.nz>
2019-06-26 16:38:05 +1200

    Add fisher's exact

--- test_noise.py
+++ test_noise.py
@@ -6,29 +6,32 @@ from skmultiflow.drift_detection.adwin import ADWIN
 from skmultiflow.evaluation import EvaluatePrequential
 from skmultiflow.data import LEDGeneratorDrift
 from skmultiflow.data import ConceptDriftStream
-from matplotlib.figure import Figure
+
+from scipy.stats import fisher_exact
 
 import matplotlib
 matplotlib.rcParams["backend"] = "Qt4Agg"
 
-stream_1 = LEDGeneratorDrift(random_state=0,
-        noise_percentage=0.1,
-        has_noise=True,
-        n_drift_features=0)
+def prepare_stream(noise_1 = 0.1, noise_2 = 0.1):
+    stream_1 = LEDGeneratorDrift(random_state=0,
+            noise_percentage=noise_1,
+            has_noise=True,
+            n_drift_features=0)
 
-stream_2 = LEDGeneratorDrift(random_state=0,
-        noise_percentage=0.3,
-        has_noise=True,
-        n_drift_features=0)
+    stream_2 = LEDGeneratorDrift(random_state=0,
+            noise_percentage=noise_2,
+            has_noise=True,
+            n_drift_features=0)
 
-stream = ConceptDriftStream(
-        stream=stream_1,
-        drift_stream=stream_2,
-        random_state=None,
-        position=10000,
-        width=1)
+    stream = ConceptDriftStream(
+            stream=stream_1,
+            drift_stream=stream_2,
+            random_state=None,
+            position=10000,
+            width=1)
 
-stream.prepare_for_use()
+    stream.prepare_for_use()
+    return stream
 
 max_samples = 20000
 pretrain_size = 200
@@ -37,6 +40,7 @@ n_wait = 200
 learner = HoeffdingTree()
 learner_drift = HoeffdingTree()
 adwin = ADWIN()
+stream = prepare_stream(noise_2=0.3)
 
 if pretrain_size > 0:
     X, y = stream.next_sample(pretrain_size)
@@ -48,7 +52,10 @@ correct = 0
 drift_detected = False
 drift_correct = 0
 
-while count < max_samples:
+contingency_table = [[0]*2 for i in range(2)]
+
+for count in range(0, max_samples):
+
     X, y = stream.next_sample()
 
     # test
@@ -63,6 +70,7 @@ while count < max_samples:
 
     if adwin.detected_change():
         drift_detected = True
+        contingency_table = [[0]*2 for i in range(2)]
         print(f"Drift detected at {count}")
 
     # train
@@ -76,7 +84,16 @@ while count < max_samples:
         drift_accuracy = drift_correct / n_wait
         print(f"{count},{accuracy},{drift_accuracy}")
 
+        if drift_detected:
+            contingency_table[0][0] += correct;
+            contingency_table[0][1] += n_wait - correct;
+
+            contingency_table[1][0] += drift_correct;
+            contingency_table[1][1] += n_wait - drift_correct;
+
         correct = 0
         drift_correct = 0
 
-    count += 1
+oddsratio, pvalue = fisher_exact(contingency_table)
+print(contingency_table)
+print(f"fisher's exact: {pvalue}")
