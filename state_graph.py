6eb9946e470e196aae60617ace121e37c6e9b42d
ingako <ingako@protonmail.com>
2019-08-30 16:53:14 +1200

    Implement state transition graph

--- state_graph.py
+++ state_graph.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 
+from random import randrange
+from pprint import pformat
+
 class LossyStateGraph:
 
-    def __init__(self, capacity):
+    def __init__(self, capacity, window_len):
         self.graph = [None] * capacity
         self.capacity = capacity
         self.is_stable = False
 
-    def get_next_tree_id(self):
-        pass
+        self.drift_counter = 0
+        self.window_len = window_len
+
+    def get_next_tree_id(self, src):
+        r = randrange(self.graph[src].total_weight)
+        sum = 0
+
+        for key, val in self.graph[src].neighbors.items():
+            sum += val[0]
+            if r < sum:
+                return key
+
+        return -1
 
     def add_node(self, key):
-        self.graph[key] = Node(key, self.capacity)
+        self.graph[key] = Node(key)
 
     def add_edge(self, src, dest):
         if self.graph[src] == None:
@@ -19,34 +33,50 @@ class LossyStateGraph:
         if self.graph[dest] == None:
             self.add_node(dest)
 
-        self.graph[src].neighbors.append(dest)
+        src_node = self.graph[src]
+        src_node.total_weight += 1
+
+        if dest not in src_node.neighbors.keys():
+            src_node.neighbors[dest] = [0, 0]
+
+        self.graph[src].neighbors[dest][0] += 1
+        self.graph[src].neighbors[dest][1] = True
 
     def remove_edge(self):
         pass
 
     def __str__(self):
-        pass
+        strs = []
+        for i in range(0, self.capacity):
+            if self.graph[i] == None:
+                continue
+            strs.append(f"Node {i}, total_weight={self.graph[i].total_weight}")
+            strs.append(pformat(self.graph[i].neighbors))
+
+        return '\n'.join(strs)
 
     def __repr__(self):
         return self.__str__()
 
 class Node:
-    def __init__(self, key, capacity):
+    def __init__(self, key):
         self.key = key
-        self.neighbors = []
-        self.counts = []
-        self.window_counts = []
-        self.total_count = 0
+        self.neighbors = dict() # <tree_id, [weight, is_hit]>
+        self.total_weight = 0
 
 if __name__ == '__main__':
-    state_graph = LossyStateGraph(5)
+    state_graph = LossyStateGraph(5, 5)
 
     state_graph.add_edge(0, 1)
     state_graph.add_edge(0, 4)
     state_graph.add_edge(1, 2)
     state_graph.add_edge(1, 3)
     state_graph.add_edge(1, 4)
+    state_graph.add_edge(1, 4)
+    state_graph.add_edge(1, 4)
+    state_graph.add_edge(1, 4)
     state_graph.add_edge(2, 3)
     state_graph.add_edge(3, 4)
 
-    # print(str(state_graph))
+    print(state_graph.get_next_tree_id(1))
+    print(str(state_graph))
