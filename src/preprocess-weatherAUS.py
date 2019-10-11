973f8ca1bf44a5a3aee26222183c0f8546510346
ingako <ingako@protonmail.com>
2019-10-11 15:03:46 +1300

    Add preprocessing scripts for the weatherAUS dataset

---
+++ src/preprocess-weatherAUS.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+with open("weatherAUS-sorted.csv", 'r') as f, open("weatherAUS.csv", 'w') as out:
+    header = f.readline()
+    out.write(header)
+
+    dicts = [{} for i in range(len(header.split(',')))]
+
+    for line in f:
+        row = line.split(',')
+
+        for i in range(len(row)):
+            feature = row[i]
+
+            if feature.isdigit() or feature == 'NA':
+                continue
+
+            if feature in dicts[i]:
+                idx = dicts[i][feature]
+            else:
+                idx = len(dicts[i])
+                dicts[i][feature] = idx
+
+            row[i] = idx
+
+        out.write(','.join([str(i) for i in row]))
+        out.write('\n')
