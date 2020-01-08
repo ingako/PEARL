50b47b8f31f1d6c07fce00e2c3f6d760998f519b
ingako <ingako@protonmail.com>
2020-01-08 14:48:18 +1300

    cpp: update covtype runner example

--- run/run-covtype-cpp.sh
+++ run/run-covtype-cpp.sh
@@ -4,13 +4,15 @@ MAX_SAMPLES=10000000
 
 mkdir -p logs
 
-generator=covtype
+dataset_name=covtype
+data_format=arff
 kappa=0.4
 ed=90
 
 # ARF
-../src/main.py --max_samples $MAX_SAMPLES --generator $generator --cpp -t 60
+../src/main.py --max_samples $MAX_SAMPLES --dataset_name $dataset_name --data_format $data_format --cpp -t 60
 
 # PEARL with pattern matching only
-../src/main.py --max_samples $MAX_SAMPLES --generator $generator --cpp -t 60 -s \
+../src/main.py --max_samples $MAX_SAMPLES --dataset_name $dataset_name --data_format $data_format \
+    --cpp -t 60 -s \
     --cd_kappa_threshold $kappa --edit_distance_threshold $ed -c 120
