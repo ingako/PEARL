56ba59a6f34f28e9329bbabd41359d9ba754d95c
ingako <ingako@protonmail.com>
2020-01-18 14:58:28 +1300

    Add pearl_bindings.cpp

--- src/cpp/pro_pearl.h
+++ src/cpp/pro_pearl.h
@@ -4,8 +4,6 @@
 #include "pearl.h"
 
 #ifndef NOPYBIND
-#include <pybind11/pybind11.h>
-namespace py = pybind11;
 #endif
 
 
@@ -46,59 +44,6 @@ class pro_pearl : public pearl {
 
 #ifndef NOPYBIND
 
-PYBIND11_MODULE(pearl, m) {
-    m.doc() = "PEARL's implementation in C++"; // module docstring
-
-    py::class_<pearl>(m, "pearl")
-        .def(py::init<int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      double,
-                      double,
-                      double,
-                      double,
-                      double,
-                      bool,
-                      bool>())
-        .def_property_readonly("drift_detected", &pearl::get_drift_detected)
-        .def("get_candidate_tree_group_size", &pearl::get_candidate_tree_group_size)
-        .def("get_tree_pool_size", &pearl::get_tree_pool_size)
-        .def("init_data_source", &pearl::init_data_source)
-        .def("get_next_instance", &pearl::get_next_instance)
-        .def("process", &pearl::process)
-        .def("__repr__",
-            [](const pearl &p) {
-                return "<pearl.pearl has "
-                    + std::to_string(p.get_tree_pool_size()) + " trees>";
-            }
-         );
-
-
-    py::class_<pro_pearl, pearl>(m, "pro_pearl")
-        .def(py::init<int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      int,
-                      double,
-                      double,
-                      double,
-                      double,
-                      double >())
-        .def("find_actual_drift_point", &pro_pearl::find_actual_drift_point)
-        .def("select_candidate_trees_proactively", &pro_pearl::select_candidate_trees_proactively)
-        .def("adapt_state_proactively", &pro_pearl::adapt_state_proactively)
-        .def("process", &pro_pearl::process)
-        .def("adapt_state", &pro_pearl::adapt_state);
-}
 
 #endif
 
