5c1dbc6ca4cd976e5c2c5a5431e1db48c559d343
ingako <ingako@protonmail.com>
2020-07-07 17:43:23 +1200

    Add data generator for agrawal with 6 concepts and abrupt drifts

---
+++ data/generate-agrawal-6.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from stream_generator import *
+
+max_samples = 400001
+
+generator = 'agrawal'
+concepts = [4, 0, 8, 1, 2, 6]
+
+for seed in range(0, 10):
+    stream = RecurrentDriftStream(generator=generator,
+                                  concepts=concepts,
+                                  has_noise=False,
+                                  random_state=seed)
+    stream.prepare_for_use()
+    print(stream.get_data_info())
+
+    output_filename = f'agrawal-6/agrawal-6-{seed}.csv'
+    # output_filename = f'agrawal-6-temp/agrawal-6-temp-{seed}.csv'
+    with open(output_filename, 'w') as out:
+        for _ in range(max_samples):
+            X, y = stream.next_sample()
+
+            out.write(','.join(str(v) for v in X[0]))
+            out.write(f',{y[0]}')
+            out.write('\n')
